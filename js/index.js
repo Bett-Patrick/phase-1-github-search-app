7c23a5858cfc1dbb6f9b45c4fe51dd333bd8b208
Bett <bettpatrick080@gmail.com>
2024-01-26 15:52:22 +0300

    final solution

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     let url = ""
     getUserInput()
+
+    // Fetch users with default search value when the page loads
+    url = "https://api.github.com/search/users?q=octocat";
+    fetchUsers(url);
+
+    //function to get user input
     function getUserInput (){
         let searchForm = document.querySelector("#github-form")
         searchForm.addEventListener("submit",(e)=>{
@@ -9,10 +15,11 @@ document.addEventListener("DOMContentLoaded", ()=>{
             url = `https://api.github.com/search/users?q=${searchInput}`
             document.querySelector("#user-list").innerHTML = ""
             fetchUsers(url)
+            document.querySelector("#search").value = ""
         })
     }
 
-
+    // function to fetch users based matching search input
     function fetchUsers(url){
         fetch(url,{
         method: "GET",
@@ -25,21 +32,71 @@ document.addEventListener("DOMContentLoaded", ()=>{
         .then((data)=>{
             let users = data.items
             users.forEach((userObject)=>{
-                console.log(userObject.login)
-                console.log(userObject.repos_url)
-                renderMatchingUsers(userObject.login)
-                // renderRepos(userObject.repos_url)
+                // console.log(userObject.login)
+                // console.log(userObject.repos_url)
+                renderMatchingUsers(userObject)
             })
         })
     } 
 
-    function renderMatchingUsers(user){
+    // function to render users based matching search input
+    function renderMatchingUsers(userObject){
         
         const userList = document.querySelector("#user-list")
-        const userContainer = document.createElement("li")
+        const userContainer = document.createElement("div")
         userContainer.className = "user-container"
-        userContainer.textContent = user
+        userContainer.innerHTML = ` 
+            <img src = "${userObject.avatar_url}"></img>
+            <div id = "avatar-name">
+                <span>${userObject.login}</span>
+            <a href =${userObject.html_url} target = "_blank">View Profile</a>
+            <button id = "repos-btn">View Repos</button>
+            </div>
+        `
         userList.appendChild(userContainer)
+        const reposBtn = userContainer.querySelector("#repos-btn")
+        reposBtn.addEventListener("click", ()=>{
+            fetchRepos(userObject.login)
+        })
+    }
+
+    //function to fetch clicked user's repos
+    function fetchRepos(userName){
+        console.log(userName)
+        fetch(`https://api.github.com/users/${userName}/repos`,{
+            method:"GET",
+            headers:{
+                "Content-Type": "application/json",
+                Accept: "application/vnd.github.v3+json"
+            }
+        })
+        .then(res=>res.json())
+        .then((reposArray)=>{
+            // console.log(data)
+            reposArray.forEach((repoObject)=>renderRepos(repoObject))
+        })
+    }
+
+    //function to render clicked user's repos
+    function renderRepos(repoObject){
+        const reposList = document.querySelector("#repos-list")
+        const owner =document.querySelector(".owner")
+        owner.textContent = `${repoObject.owner.login}'s Repositories`
+
+        const repoContainer = document.createElement("li")
+        repoContainer.className = "repo-container"
+        repoContainer.innerHTML = `
+            <img src = ${repoObject.owner.avatar_url}></img>
+            <div class = "flex-details">
+                <p><strong>name:</strong> ${repoObject.name}</p>
+                <p><strong>visibility:</strong> ${repoObject.visibility}</p
+                <p><strong>description:</strong> ${repoObject.description}</p>
+                <p><strong>language:</strong> ${repoObject.language}</p>
+                <p><strong>repo link:</strong> <a href = "${repoObject.html_url}" target = "_blank">${repoObject.html_url}</p>
+            </div>
+        `
+        document.querySelector("#user-list").style.display = "none"
+        reposList.appendChild(repoContainer)
     }
     
 })
